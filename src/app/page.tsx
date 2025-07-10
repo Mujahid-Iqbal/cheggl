3ac692c181c8d8c5c4c98a5dbf1a1feed71d6ca0
Mujahid-Iqbal <mujahid7014@gmail..com>
2025-07-10 22:45:45 +0500

    add component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeatureCards from './components/FeatureCards'
 import HeroCarousel from './components/HeroCarousel'
 import HowItWorks from './components/HowItWorks'
+import Testimonials from './components/Testimonials'
 
 export default function HomePage() {
   return (
@@ -8,7 +9,7 @@ export default function HomePage() {
       <HeroCarousel />
       <FeatureCards />
       <HowItWorks />
-
+      <Testimonials />
       {/* other sections */}
     </>
   )
